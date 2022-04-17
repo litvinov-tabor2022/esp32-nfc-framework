d203b9f65e14bdf90e962f5904dc1f40c41c7b0e
Matěj <jehlicka@mjehlicka.eu>
2022-04-17 23:14:29 +0200

    Skills utils

--- src/portal_defs.h
+++ src/portal_defs.h
@@ -6,11 +6,11 @@
 struct Transaction {
     u64 time;
     u8 user_id;
-    i8 strength;
-    i8 magic = 0;
-    i8 dexterity = 0;
-    i8 bonus_points = 0;
-    i8 skill = 0;
+    u8 strength;
+    u8 magic = 0;
+    u8 dexterity = 0;
+    i16 skill = 0;
+    u8 bonus_points = 0;
 };
 
 #endif //ESP32_PORTAL_DEFS_H
